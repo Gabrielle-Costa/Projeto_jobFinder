8a7c060736c557b5775d2ed63b5edd7cb40f3630
Gabrielle-Costa <gabbytargino@yahoo.com.br>
2023-02-15 17:43:29 -0300

    Configuração da conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express    = require('express');
 const app        = express();
+const db = require('./db/connection')
 
 const PORT = 3000;
 
@@ -7,6 +8,15 @@ app.listen(PORT, function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.send("Está funcionando bb");
+//conexão com o bando de dados
+db.authenticate()
+.then(()=>{console.log("Conectou ao BD!")})
+.catch(err =>{
+    console.log("ocorreu um erro ao conectar com o BD", err);
 });
+
+//rotas
+app.get('/', (req, res) => {
+    res.send("Está funcionando");
+});
+
