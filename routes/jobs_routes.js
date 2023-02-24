dbf4c2359d5289020652ff9280df259dcb312492
Gabrielle-Costa <gabbytargino@yahoo.com.br>
2023-02-24 18:04:20 -0300

    Criado a logica de busca e o acesso às informações da vaga

--- routes/jobs_routes.js
+++ routes/jobs_routes.js
@@ -2,18 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
-router.get('/test', (req, res)=>{
+router.get('/test', (req, res) => {
     res.send('deu certo');
 });
 
-router.get('/add',(req,res)=>{
+//detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+        where: {id: req.params.id}
+    }).then(job => {
+        res.render('view', {
+            job
+        });
+}).catch(err => console.log(err)));
+
+//form da rota de envio
+router.get('/add', (req, res) => {
     res.render('add');
 });
 
-//add jpb via post
-router.post('/add', (req, res)=>{
-    
-    let {title, salary, company, description, email, new_job} = req.body;
+//add job via post
+router.post('/add', (req, res) => {
+
+    let { title, salary, company, description, email, new_job } = req.body;
 
     //Inserir dados
     Job.create({
@@ -24,8 +34,8 @@ router.post('/add', (req, res)=>{
         email,
         new_job
     })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err));
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
